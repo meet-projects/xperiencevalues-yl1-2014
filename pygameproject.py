ea3a817b4757af6695f22360a5dd5b58f6e207be
Wassim Saba <wassims13@meet.mit.edu>
2014-04-10 17:21:28 +0300

    first page

--- pygameproject.py
+++ pygameproject.py
@@ -1,20 +1,37 @@
 import pygame
+import buttonclass
 
 if __name__=="__main__":
-	pygame.init()
-	main_screen = pygame.display.set_mode((400, 400))
-	main_screen.fill((255,255,255))
-	button_rec = pygame.Rect(1,1,3000,3000)
-	button_su = pygame.Surface([20, 20])
-	button_su.fill((34,100,100))
-	main_screen.blit(button_su, button_rec)
+    pygame.init()
+    main_screen = pygame.display.set_mode((400, 400))
+    main_screen.fill((255,255,255))
+    myfont = pygame.font.SysFont("ComicSansMS", 48)
+    label = myfont.render("Pick a value:", 1, (11,46,7))
+    main_screen.blit(label, (100, 20))
+    button=buttonclass.button(115, 70,20,100)
+    button.draw(main_screen)
+    myfont = pygame.font.SysFont("ComicSansMS", 18)
+    label = myfont.render(" Lead by example", 1, (11,206,7))
+    main_screen.blit(label, (22, 120))
+    button2=buttonclass.button(115, 70,320,100)
+    button2.draw(main_screen)
+    myfont = pygame.font.SysFont("ComicSansMS", 18)
+    label = myfont.render("Equality", 1, (11,206,7))
+    main_screen.blit(label, (22, 120))
+    button3=buttonclass.button(115, 70,165,240)
+    button3.draw(main_screen)
+    myfont = pygame.font.SysFont("ComicSansMS", 18)
+    label = myfont.render("Team work", 1, (11,206,7))
+    main_screen.blit(label, (22, 120))
 
-while True: 
+
+
+    while True: 
         ev = pygame.event.poll()
        
         if ev.type == pygame.MOUSEBUTTONDOWN:
             x, y = ev.pos
-            if button_rec.collidepoint(x, y):
-                print "you clicked me!"
+            # if button_rec.collidepoint(x, y):
+                # print "you clicked me!"
         pygame.display.flip()
     
