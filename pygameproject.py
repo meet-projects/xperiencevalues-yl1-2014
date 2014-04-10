64696e951fbe5823cdc6c2369433dd5e15cfaf60
Guest <guest-1pBm2P@meet.mit.edu>
2014-04-10 17:23:29 +0300

    play

--- pygameproject.py
+++ pygameproject.py
@@ -1,15 +1,16 @@
 import pygame
 
 if __name__=="__main__":
-	pygame.init()
-	main_screen = pygame.display.set_mode((400, 400))
-	main_screen.fill((255,255,255))
-	button_rec = pygame.Rect(1,1,3000,3000)
-	button_su = pygame.Surface([20, 20])
-	button_su.fill((34,100,100))
-	main_screen.blit(button_su, button_rec)
+    pygame.init()
+    main_screen = pygame.display.set_mode((400, 400))
+    main_screen.fill((255,255,255))
+    button_rec = pygame.Rect(1,1,3000,3000)
+    button_su = pygame.Surface([20, 20])
+    button_su.fill((34,100,100))
+    main_screen.blit(button_su, button_rec)
 
-while True: 
+
+    while True: 
         ev = pygame.event.poll()
        
         if ev.type == pygame.MOUSEBUTTONDOWN:
